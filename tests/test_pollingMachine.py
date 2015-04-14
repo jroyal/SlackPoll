35a3ec38c32949a744810571c75edb3be74b621d
James Royal <jhroyal@us.ibm.com>
2015-04-14 11:16:41 -0500

    Fixed some of the unittests

--- tests/test_pollingMachine.py
+++ tests/test_pollingMachine.py
@@ -6,27 +6,27 @@ __author__ = 'James Royal'
 
 class TestPollingMachine(TestCase):
     def test_create_poll_no_other_poll(self):
-        pm = PollingMachine()
+        pm = PollingMachine("abc.com")
         poll = pm.create_poll("James", "#jamestestchannel", "Test topic", {"option1": 0, "option2": 0})
         assert isinstance(poll, Poll)
         assert len(pm.active_polls) == 1
 
     def test_create_poll_existing_different_channel_poll(self):
-        pm = PollingMachine()
+        pm = PollingMachine("abc.com")
         pm.create_poll("James", "#jamestestchannel", "Test topic", {"option1": 0, "option2": 0})
         poll = pm.create_poll("James", "#jamestestchannel2", "Test topic", {"option1": 0, "option2": 0})
         assert isinstance(poll, Poll)
         assert len(pm.active_polls) == 2
 
     def test_create_poll_active_poll_in_channel(self):
-        pm = PollingMachine()
+        pm = PollingMachine("abc.com")
         pm.create_poll("James", "#jamestestchannel", "Test topic", {"option1": 0, "option2": 0})
         poll = pm.create_poll("James", "#jamestestchannel", "Test topic", {"option1": 0, "option2": 0})
         assert poll is None
         assert len(pm.active_polls) == 1
 
     def test_create_poll_after_original_poll_closed(self):
-        pm = PollingMachine()
+        pm = PollingMachine("abc.com")
         pm.create_poll("James", "#jamestestchannel", "Test topic", {"option1": 0, "option2": 0})
         pm.close_poll("James", "#jamestestchannel")
         poll = pm.create_poll("James", "#jamestestchannel", "Test topic", {"option1": 0, "option2": 0})
@@ -34,34 +34,34 @@ class TestPollingMachine(TestCase):
         assert len(pm.active_polls) == 1
 
     def test_close_poll(self):
-        pm = PollingMachine()
+        pm = PollingMachine("abc.com")
         pm.create_poll("James", "#jamestestchannel", "Test topic", {"option1": 0, "option2": 0})
         poll = pm.close_poll("James", "#jamestestchannel")
         assert isinstance(poll, Poll)
         assert len(pm.active_polls) == 0
 
     def test_close_poll_wrong_user(self):
-        pm = PollingMachine()
+        pm = PollingMachine("abc.com")
         pm.create_poll("James", "#jamestestchannel", "Test topic", {"option1": 0, "option2": 0})
         poll = pm.close_poll("Lisa", "#jamestestchannel")
         assert isinstance(poll, basestring)
         assert len(pm.active_polls) == 1
 
     def test_close_poll_no_existing_poll(self):
-        pm = PollingMachine()
+        pm = PollingMachine("abc.com")
         poll = pm.close_poll("Lisa", "#jamestestchannel")
         assert isinstance(poll, basestring)
         assert len(pm.active_polls) == 0
 
     def test_vote(self):
-        pm = PollingMachine()
+        pm = PollingMachine("abc.com")
         pm.create_poll("James", "#jamestestchannel", "Test topic", {"option1": 0, "option2": 0})
-        output = pm.vote("#jamestestchannel", 1)
+        output = pm.vote("#jamestestchannel", "james", 1)
         assert pm.active_polls["#jamestestchannel"].num_casted_votes == 1
         assert isinstance(output, basestring)
 
     def test_get_num_of_casted_votes(self):
-        pm = PollingMachine()
+        pm = PollingMachine("abc.com")
         pm.create_poll("James", "#jamestestchannel", "Test topic", {"option1": 0, "option2": 0})
-        output = pm.vote("#jamestestchannel", 1)
+        output = pm.vote("#jamestestchannel", "james", 1)
         assert pm.get_num_of_casted_votes("#jamestestchannel") == 1
